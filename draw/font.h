79c31d95ccba5a5b7ab0140ef13d1013b1c3af48
Markus Heimerl <149831926+markusheimerl@users.noreply.github.com>
2023-11-28 19:46:12 +0000

    Remove unused font, bmp, and primitive files

--- draw/font.h
+++
@@ -1,6 +0,0 @@
-#ifndef FONT_H
-#define FONT_H
-
-void draw_char(unsigned char *arr, int width, int height, int x, int y, unsigned char character, unsigned char color, int scale);
-
-#endif // FONT_H
