3ef8b96f9fd2f69c5bcf59f55cd0f122a5218577
Markus Heimerl <149831926+markusheimerl@users.noreply.github.com>
2023-12-16 18:55:11 +0000

    Add dynamic memory allocation for network and layers

--- nn/network.c
+++ nn/network.c
@@ -1,5 +1,6 @@
 #include "network.h"
 #include "layer.h"
+#include <stdlib.h>
 
 struct network_
 {
@@ -7,6 +8,17 @@ struct network_
     layer *layers;
 };
 
-network Network(unsigned int size, unsigned int *layer_sizes, operation layer_acts, operation layer_act_derivs)
+network Network(unsigned int network_size, unsigned int *layer_sizes, operation *layer_acts, operation *layer_act_derivs)
 {
+    network net = malloc(sizeof(struct network_));
+    net->size = network_size;
+    net->layers = calloc(network_size, sizeof(layer));
+
+    net->layers[0] = Layer(NULL, layer_sizes[0], 0, layer_acts[0], layer_act_derivs[0]);
+    for (unsigned int i = 1; i < network_size; i++)
+    {
+        net->layers[i] = Layer(net->layers[i - 1], layer_sizes[i], layer_sizes[i - 1], layer_acts[i], layer_act_derivs[i]);
+    }
+
+    return net;
 }
--- nn/network.h
+++ nn/network.h
@@ -5,6 +5,6 @@
 
 typedef struct network_ *network;
 
-network Network(unsigned int size, unsigned int *layer_sizes, operation layer_acts, operation layer_act_derivs);
+network Network(unsigned int network_size, unsigned int *layer_sizes, operation *layer_acts, operation *layer_act_derivs);
 
 #endif // NETWORK_H
